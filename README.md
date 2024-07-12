5f9c4e4fcb71a3d43a45552bd289b19ced0d77a2
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-07-12 20:40:49 -0300

    feat: it creates all project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Number Guessing Game
+
+Welcome to the Random Number Guessing Game! This interactive game generates a random number between 1 and 100, and your task is to guess the number. After each guess, the program will give you feedback: "TOO HIGH!", "TOO LOW!", or "CORRECT!!!". Keep guessing until you find the correct number. Enjoy the challenge!
---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+  cout << " **********GUESSING THE NUMBER**********\n";
+
+  int tries = 0;
+  int actualTry;
+
+  srand(time(0));
+  int randomNum = (rand() % 100) + 1;
+
+  do
+  {
+    cout << " Type a number between 1-100 to guess the number: ";
+    cin >> actualTry;
+    tries++;
+
+    if (actualTry > randomNum)
+    {
+      cout << " TOO HIGH!\n";
+    }
+    else if (actualTry < randomNum)
+    {
+      cout << " TOO LOW!\n";
+    }
+    else
+      cout << " CORRECT!!!\n";
+
+  } while (actualTry != randomNum);
+
+  return 0;
+}
